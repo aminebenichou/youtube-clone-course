4375e77ba4f04955ddf0e2c8d789d8add57783b9
Amine Benichou <aminemohammed.benichou@gmail.com>
2023-03-25 14:42:41 +0100

    vid card

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
 var side_button = document.getElementsByClassName("btn-side");
-side.forEach(element => {
-    if element.
-});
+const btn = document.getElementById("btn")
+btn.addEventListener("click", (e) => console.log("hey"))
+
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'd6f2791a72msha295b982405ced2p113a58jsn1c774b87e870',
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+    }
+};
+var thumbnails_resp;
+var i = 0
+fetch('https://youtube-v31.p.rapidapi.com/search?q=music&part=snippet%2Cid&regionCode=US&maxResults=50&order=date', options)
+    .then(response => response.json())
+    .then(response => {
+        while (i < response.items.length) {
+            thumbnails_resp = response.items[0].snippet.thumbnails.default.url
+            i++
+
+            var thumbnails = document.getElementsByClassName("thumbnail")
+            var j = 0;
+            while (j < thumbnails.length) {
+                thumbnails[j].src = thumbnails_resp
+                console.log(thumbnails_resp)
+                j++
+            }
+        }
+    })
+    .catch(err => console.error(err));
+
